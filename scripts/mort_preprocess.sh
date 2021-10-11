b835940adae8c7a8f2960af4de85057616495ee9
Gautham-G <47530745+Gautham-G@users.noreply.github.com>
2021-10-10 22:49:04 -0400

    Create mort_preprocess.sh

---
+++ scripts/mort_preprocess.sh
@@ -0,0 +1,9 @@
+set -e
+mkdir -p ./data/covid_data/saves
+declare -a regs=('AL' 'AK' 'AZ' 'AR' 'CA' 'CO' 'CT' 'DE' 'DC' 'FL' 'GA' 'ID' 'IL' 'IN' 'IA' 'KS' 'KY' 'LA' 'ME' 'MD' 'MA' 'MI' 'MN' 'MS' 'MO' 'MT' 'NE' 'NV' 'NH' 'NJ' 'NM' 'NY' 'NC' 'ND' 'OH' 'OK' 'OR' 'PA' 'RI' 'SC' 'SD' 'TN' 'TX' 'UT' 'VT' 'VA' 'WA' 'WV' 'WI' 'WY' 'X')
+
+for r in "${regs[@]}"
+do
+    echo "$r"
+    python ./covid_extract/covid_single.py -r "$r" -e $1 -s 0
+done 
