154912ed48ac857ce6e14fdadae0a7dd7785d55e
Gautham-G <47530745+Gautham-G@users.noreply.github.com>
2021-10-30 16:15:45 -0400

    Update train_3.sh

--- train_sript/train_3.sh
+++ train_sript/train_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 11 12 13 14 15 
   do 
-  	python "train_hosp.py -e 202142 -m deploy_week_42_"$i" -c True -d "$i" --start_model hosp_deploy_week_41_"$i
+  	python train_hosp.py -e 202142 -m deploy_week_42_$i -c True -d $i --start_model hosp_deploy_week_41_$i
   done
