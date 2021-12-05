997d4605f9e6e2f7d848b12f91add8640c97fc14
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2021-12-05 15:31:57 -0500

    48

--- train_sript/train_2.sh
+++ train_sript/train_2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for i in 6 7 8 9 10  
   do 
-    python train_hosp.py -e 202148 -m deploy_week_48_$i -c True -d $i  —start_model hosp_deploy_week_47_$i
+    python train_hosp.py -e 202148 -m deploy_week_48_$i -c True -d $i 
+#     —start_model hosp_deploy_week_47_$i
   done
