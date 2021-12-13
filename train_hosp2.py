712200b555ba1ddb56c7abd96d590c47aa5508ed
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2021-12-13 07:50:12 -0500

    Update train_hosp2.py

--- train_hosp2.py
+++ train_hosp2.py
@@ -31,7 +31,7 @@ parser.add_option("-m", "--save", dest="save_model", default="default", type="st
 parser.add_option("--start_model", dest="start_model", default="None", type="string")
 parser.add_option("--samples", dest="samples_past", default=14, type="int")
 parser.add_option("-c", "--cuda", dest="cuda", default=True, action="store_true")
-parser.add_option("--samplesmean", dest="samplesmean", default=False, action="store_true")
+parser.add_option("--samplesmean", dest="samplesmean", default=True, action="store_true")
 parser.add_option("--start", dest="start_day", default=-120, type="int")
 
 (options, args) = parser.parse_args()
